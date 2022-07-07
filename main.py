8f18ba9ed2d1eec92a71976ccfd8f61c4d81a72d
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 21:02:49 +0100

    Multiple Points

--- main.py
+++ main.py
@@ -14,3 +14,9 @@ xpoints = np.array([1, 8])
 ypoints = np.array([3, 10])
 plt.plot(xpoints, ypoints, 'o')
 plt.show()
+
+# Multiple Points
+xpoints = np.array([1, 2, 6, 8])
+ypoints = np.array([3, 8, 1, 10])
+plt.plot(xpoints, ypoints)
+plt.show()

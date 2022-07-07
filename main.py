c9197ea3c6f9054b4f0360e0c49d32847188468f
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 21:26:20 +0100

    Adding Grid Line

--- main.py
+++ main.py
@@ -76,6 +76,20 @@ plt.show()
 x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
 y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
 plt.plot(x, y)
+font1 = {'family':'serif','color':'blue','size':20}
+font2 = {'family':'serif','color':'darkred','size':15}
+plt.title("Sports Watch Data", fontdict = font1)
+plt.xlabel("Average Pulse", fontdict = font2)
+plt.ylabel("Calorie Burnage", fontdict = font2)
+plt.show()
+
+plt.title("Sports Watch Data", loc = 'left')
 plt.xlabel("Average Pulse")
 plt.ylabel("Calorie Burnage")
+plt.plot(x, y)
+plt.show()
+
+# Adding Grid Line
+plt.plot(x, y)
+plt.grid()
 plt.show()

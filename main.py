d925e91bff8a33de3e28f8ff0c239b65c4c223da
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 21:23:42 +0100

    Labels for a Plot

--- main.py
+++ main.py
@@ -48,8 +48,34 @@ plt.show()
 plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
 plt.show()
 # plt.plot(ypoints, marker = 'o', ms = 20, mec = '#4CAF50', mfc = '#4CAF50')
-
-# Line
-plt.plot(ypoints, linestyle = 'dotted')
+plt.plot(ypoints, color = 'r') # plt.plot(ypoints, c = '#4CAF50')
+plt.show()
+plt.plot(ypoints, linewidth = '20.5')
 plt.show()
 
+# Line
+plt.plot(ypoints, linestyle = 'dotted') # Or plt.plot(ypoints, ls = ':')
+plt.show()
+# plt.plot(ypoints, linestyle = 'dashed')
+
+# Multiple Lines
+y1 = np.array([3, 8, 1, 10])
+y2 = np.array([6, 2, 7, 11])
+plt.plot(y1)
+plt.plot(y2)
+plt.show()
+
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
+plt.plot(x1, y1, x2, y2)
+plt.show()
+
+# Labels for a Plot
+x = np.array([80, 85, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+plt.plot(x, y)
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+plt.show()

fdaa0750e5f4131f13b9c148bb0d037d07465b71
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 21:19:32 +0100

    Line

--- main.py
+++ main.py
@@ -45,3 +45,11 @@ plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r')
 plt.show()
 plt.plot(ypoints, marker = 'o', ms = 20, mfc = 'r')
 plt.show()
+plt.plot(ypoints, marker = 'o', ms = 20, mec = 'r', mfc = 'r')
+plt.show()
+# plt.plot(ypoints, marker = 'o', ms = 20, mec = '#4CAF50', mfc = '#4CAF50')
+
+# Line
+plt.plot(ypoints, linestyle = 'dotted')
+plt.show()
+

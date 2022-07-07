fe27e3c1cc1f0f85fc0fe9a1b1a6f518dab9f25b
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 21:56:55 +0100

    Attributes on Pie Chart

--- main.py
+++ main.py
@@ -263,6 +263,27 @@ x = np.random.normal(170, 10, 250)
 plt.hist(x)
 plt.show()
 
+#Pie Chart
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+plt.pie(y, labels = mylabels)
+plt.show() 
+# Start Angle
+plt.pie(y, labels = mylabels, startangle = 90)
+plt.show() 
+# Explode
+myexplode = [0.2, 0, 0, 0]
+plt.pie(y, labels = mylabels, explode = myexplode)
+plt.show() 
+# Shadow
+myexplode = [0.2, 0, 0, 0]
+plt.pie(y, labels = mylabels, explode = myexplode, shadow = True)
+plt.show() 
+# Color
+mycolors = ["black", "hotpink", "b", "#4CAF50"]
+plt.pie(y, labels = mylabels, colors = mycolors)
+plt.legend(title = "Four Fruits:")
+plt.show() 
 
 
 

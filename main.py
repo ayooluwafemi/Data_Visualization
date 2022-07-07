29315795ecaecd2574db9f956fd701e3447d9008
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 20:57:06 +0100

    Pyplot

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+# Matplotlib is a low level graph plotting library in python that serves as a visualization utility
+# import matplotlib
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import matplotlib.pyplot as plt
+import numpy as np
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+plt.plot(xpoints, ypoints)
+plt.show()

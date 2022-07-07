bd1467533230032e85a4533920e5666ae09bb34b
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 21:01:54 +0100

    Markers

--- main.py
+++ main.py
@@ -8,3 +8,9 @@ xpoints = np.array([0, 6])
 ypoints = np.array([0, 250])
 plt.plot(xpoints, ypoints)
 plt.show()
+
+# Plot with Markers
+xpoints = np.array([1, 8])
+ypoints = np.array([3, 10])
+plt.plot(xpoints, ypoints, 'o')
+plt.show()

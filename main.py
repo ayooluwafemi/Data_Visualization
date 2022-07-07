6c63de4eea9474a745a3cbe7857854d16630bb81
Ayo Oluwafemi <eoluwafemi76@yahoo.com>
2022-07-07 21:33:22 +0100

    Title

--- main.py
+++ main.py
@@ -157,6 +157,20 @@ plt.subplot(2, 3, 6)
 plt.plot(x,y)
 plt.show()
 
+# Title
+#plot 1:
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+plt.subplot(1, 2, 1)
+plt.plot(x,y)
+plt.title("SALES")
+#plot 2:
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+plt.subplot(1, 2, 2)
+plt.plot(x,y)
+plt.title("INCOME")
+plt.show()
 
 
 
